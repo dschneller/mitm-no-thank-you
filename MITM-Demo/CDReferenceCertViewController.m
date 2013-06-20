e0150bd4f3eb109cd44c87a06fbd185e15f82342
Daniel Schneller <ds@danielschneller.de>
2013-06-20 17:40:05 +0200

    Improve code comments. Simplify reference comparison.

--- MITM-Demo/CDReferenceCertViewController.m
+++ MITM-Demo/CDReferenceCertViewController.m
@@ -9,60 +9,36 @@
 #import "CDReferenceCertViewController.h"
 
 @interface CDReferenceCertViewController ()
-{
-    NSUInteger _numReferenceCerts;
-    CFArrayRef* _chainsOfTrust;
-    SecCertificateRef* _secCertificateRefs;
-    CFDataRef* _certDataRefs;
-}
+
+@property (nonatomic, readonly) NSArray* referenceCerts;
 
 @end
 
 @implementation CDReferenceCertViewController
+@synthesize referenceCerts = _referenceCerts;
 
-- (void) viewDidLoad
+- (NSArray *)referenceCerts
 {
-    [super viewDidLoad];
-    [self loadReferenceCertificates];
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        // Load reference certificate from our main bundle
+        NSMutableArray* certDatas = [NSMutableArray array];
+        NSArray* certificateFiles = @[@"star.centerdevice.de", @"www.centerdevice.de"];
+        for (NSUInteger i = 0; i<certificateFiles.count; i++)
+        {
+            NSString *certificateFileLocation = [[NSBundle mainBundle] pathForResource:certificateFiles[i] ofType:@"der"];
+            NSData *certificateData = [[NSData alloc] initWithContentsOfFile:certificateFileLocation];
+            [certDatas addObject:certificateData];
+        }
+        _referenceCerts = [NSArray arrayWithArray:certDatas];
+    });
+    return _referenceCerts;
 }
-
-- (void) loadReferenceCertificates
-{
-    // Load reference certificate from our main bundle
-    NSArray* certificateFiles = @[@"star.centerdevice.de", @"www.centerdevice.de"];
-    _numReferenceCerts = certificateFiles.count;
-    if (_numReferenceCerts == 0) { return; }
-    
-    _chainsOfTrust = malloc(_numReferenceCerts * sizeof(CFArrayRef));
-    _secCertificateRefs = malloc(_numReferenceCerts * sizeof(SecCertificateRef));
-    _certDataRefs = malloc(_numReferenceCerts * sizeof(CFDataRef));
-    
-    for (NSUInteger i = 0; i<certificateFiles.count; i++)
-    {
-        NSString *certificateFileLocation = [[NSBundle mainBundle] pathForResource:certificateFiles[i] ofType:@"der"];
-        NSData *certificateData = [[NSData alloc] initWithContentsOfFile:certificateFileLocation];
-        _certDataRefs[i] = (__bridge_retained CFDataRef)certificateData;
-        _secCertificateRefs[i] = SecCertificateCreateWithData(NULL, _certDataRefs[i]);
-        
-        // Create a specific chain of trust that just contains our reference certificate at the top
-        _chainsOfTrust[i] = CFArrayCreate(NULL, (void *)&_secCertificateRefs[i], 1, NULL);;
-    }
-}
-
-- (void) dealloc
-{
-    for (NSUInteger i=0; i<_numReferenceCerts; i++)
-    {
-        CFRelease(_chainsOfTrust[i]);
-        CFRelease(_secCertificateRefs[i]);
-        CFRelease(_certDataRefs[i]);
-    }
-}
-
 
 #pragma mark - SSL related NSURLConnectionDelegate methods
 
--(void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
+
+-(void)connection:(NSURLConnection *)connection willSendRequestForAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge
 {
     if (!self.progressController.working) { return; }
     [self.progressController appendLog:@"Authentication challenge received"];
@@ -88,43 +64,52 @@
 - (BOOL)matchesKnownCertificates:(SecTrustRef)presentedTrustInformation {
     // prepare return value
     BOOL certificateVerified = NO; // defensive default
-
-    for (NSUInteger i=0; i<_numReferenceCerts && !certificateVerified; i++)
+    
+    
+    // Perform default trust evaluation first, to make sure the format of the presented
+    // data is correct and there are no other trust issues
+    SecTrustResultType evaluationResult;
+    OSStatus status = SecTrustEvaluate(presentedTrustInformation, &evaluationResult);
+    
+    // status now contains the general success/failure result of the evaluation call.
+    // evaluationResult has the concrete outcome of the certificate check.
+    // only if the call was successful do we need to look a the actual evaluation result
+    if (status == errSecSuccess)
     {
-        [self.progressController appendLog:[NSString stringWithFormat:@"Check against ref. cert #%d", i]];
-
-        // Set up the security framework to verify the presented certificate against our custom
-        // chain of trust instead of against the system keychain
-        SecTrustSetAnchorCertificates(presentedTrustInformation, _chainsOfTrust[i]);
-
-        // Now perform the actual check, based on the set up a moment ago
-        SecTrustResultType evaluationResult;
-        OSStatus status = SecTrustEvaluate(presentedTrustInformation, &evaluationResult);
+        // only in these 2 cases the certificate could be definitely verified.
+        // now we still need to double check the received certificates by comparing
+        // them to the reference certificates. That way we make sure we are talking
+        // to the correct server, and not merely have an encrypted channel.
         
-        // status now contains the general success/failure result of the evaluation call.
-        // evaluationResult has the concrete outcome of the certificate check.
-        
-        
-        // only if the call was successful do we need to look a the actual evaluation result
-        if (status == errSecSuccess)
+        // otherwise someone could attempt a man-in-the-middle attack using
+        // a certificate that was correctly signed by any of the many root-CAs
+        // the system trusts by default.
+        if (evaluationResult == kSecTrustResultUnspecified
+            || evaluationResult == kSecTrustResultProceed)
         {
-            // only in these 2 cases the certificate could be definitely verified.
-            // now we still need to double check the certificate fingerprints to
-            // make sure we are talking to the correct server, and not merely have
-            // an encrypted channel.
-            // otherwise someone could attempt a man-in-the-middle attack using
-            // a certificate that was correctly signed by any of the many root-CAs
-            // the system trusts by default.
-            certificateVerified = (evaluationResult == kSecTrustResultUnspecified
-                                   || evaluationResult == kSecTrustResultProceed);
-            [self.progressController appendLog:[NSString stringWithFormat:@"  Verified: %@",
-                                                certificateVerified ? @"YES" : @"NO"]];
-        } else {
-            [self.progressController appendLog:@"Problem occurred executing SecTrustEvaluate. Rejecting connection."];
+            NSUInteger referenceCertCount =  [self.referenceCerts count];
+            for (NSUInteger i = 0; i<referenceCertCount && !certificateVerified; i++)
+            {
+                NSData* referenceCert = self.referenceCerts[i];
+                
+                [self.progressController appendLog:[NSString stringWithFormat:@"Check against ref. cert #%d", i]];
+                
+                CFIndex presentedCertsCount = SecTrustGetCertificateCount(presentedTrustInformation);
+                for (CFIndex j = 0; j<presentedCertsCount && !certificateVerified; j++)
+                {
+                    SecCertificateRef currentCert = SecTrustGetCertificateAtIndex(presentedTrustInformation, i);
+                    CFDataRef certData = SecCertificateCopyData(currentCert);
+                    certificateVerified = [referenceCert isEqualToData:CFBridgingRelease(certData)];
+                }
+            }
         }
-
+        [self.progressController appendLog:[NSString stringWithFormat:@"  Verified: %@",
+                                            certificateVerified ? @"YES" : @"NO"]];
+    } else {
+        [self.progressController appendLog:@"Problem occurred executing SecTrustEvaluate. Rejecting connection."];
     }
-
+    
+    
     return certificateVerified;
 }
 
